06c00d6a2cbde76262f7b9b1936269b0e4b1f210
xavialgarraa <131766832+xavialgarraa@users.noreply.github.com>
2025-07-08 16:59:59 +0200

    gh pages

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/portfolio-react",
+  base: "/portfolio-react/",
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
